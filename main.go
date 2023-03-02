50beb29864c1d7a44af10e0cf5e5bd4bb6c0b692
Jim Raden <james.raden@gmail.com>
2023-03-02 17:46:35 -0500

    Add coin toss option

--- main.go
+++ main.go
@@ -8,19 +8,35 @@ import (
 )
 
 var (
-	n int
+	n    int
+	coin bool
 )
 
 func init() {
 	flag.IntVar(&n, "n", 0, "Defines [1, n] range.\nn must be > 0.")
+	flag.BoolVar(&coin, "c", false, "Coin toss.")
+	oldUsage := flag.Usage
+	flag.Usage = func() {
+		oldUsage()
+		fmt.Fprintln(flag.CommandLine.Output(), "\nNOTE: -n and -c are mutually exclusive.")
+	}
 }
 
 func main() {
 	flag.Parse()
-	if n < 1 {
+	switch {
+	case coin && n == 0:
+		coins := []string{"heads", "tails"}
+		rand.Shuffle(len(coins), func(i, j int) {
+			coins[i], coins[j] = coins[j], coins[i]
+		})
+		fmt.Println(coins[0])
+
+	case !coin && n != 0:
+		r := rand.Intn(n-1) + 1
+		fmt.Println(r)
+	default:
 		flag.Usage()
 		os.Exit(1)
 	}
-	r := rand.Intn(n-1) + 1
-	fmt.Printf("%v\n", r)
 }
