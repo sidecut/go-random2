de59342ea9c6386cab8a87a6e3a1a4c599787f14
Jim Raden <james.raden@gmail.com>
2023-03-02 17:24:17 -0500

    Simple implementation using non-crypto RNG

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+var (
+	n int
+)
+
+func init() {
+	flag.IntVar(&n, "n", 0, "Defines [1, n] range.\nn must be > 0")
+}
+
+func main() {
+	flag.Parse()
+	if n < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	r := rand.Intn(n-1) + 1
+	fmt.Printf("%v\n", r)
+}
