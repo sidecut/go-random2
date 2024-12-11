d58eb0f0d718acab55246e664b55b64e7b4c5dc2
Jim Raden <james.raden@gmail.com>
2024-12-10 19:00:52 -0500

    Import package properly

--- Package.swift
+++ Package.swift
@@ -6,12 +6,16 @@ import PackageDescription
 let package = Package(
     name: "go-random2",
     dependencies: [
-        .package(url: "swift-argument-parser", from: "1.5.0"),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "go-random2"),
+            name: "go-random2",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ])
     ]
 )
